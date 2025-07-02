21314feebe6124b8d861ecde3805301941831bdf
Angelo Lustosa de Figueiredo Filho <Angelo.Filho@digitalcollege.internal>
2025-07-01 22:13:05 -0300

    Carregar os municipios e estados

--- script.js
+++ script.js
@@ -1,22 +1,70 @@
 
 const consultarCep = () => {
-    console.log('chamou a api')
-    const cep = document.getElementById('cep').value
+  console.log('chamou a api')
+  const cep = document.getElementById('cep').value
 
-    let uri = `https://cep.awesomeapi.com.br/json/${cep}`
+  let uri = `https://cep.awesomeapi.com.br/json/${cep}`
 
-    console.log(`URI: ${uri}`)
-    fetch(uri)
-      .then(response => response.json())
-      .then(json => {
-        console.log(json)
+  console.log(`URI: ${uri}`)
 
-        document.getElementById('logradouro').value = json.address
-        document.getElementById('bairro').value = json.district
-        document.getElementById('uf').value = json.state
+  fetch(uri)
+    .then(response => response.json())
+    .then(json => {
+      console.log(json)
+
+      document.getElementById('logradouro').value = json.address
+      document.getElementById('bairro').value = json.district
+      document.getElementById('ddd').value = json.ddd
+      document.getElementById('uf').value = json.state
+      document.getElementById('localidade').value = json.city
     })
 }
 
-/* function consultarCep() {
 
-} */
+const fetchEstados = () => {
+  let uri = `https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome`
+
+  console.log(`URI: ${uri}`)
+
+  fetch(uri)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+
+      let options = '<option selected disabled>Selecione...</option>'
+
+      data.forEach(estado => {
+        options = options + `<option value="${estado.sigla}">${estado.nome}</option>`
+      });
+
+      document.getElementById('uf').innerHTML = options 
+      //' <option value="AC">Acre (AC)</option>  <option value="AL">Alagoas (AL)</option> <option value="AP">Amapá (AP)</option>'
+
+    })
+}
+
+fetchEstados()
+
+const fetchMunicipios = (event) => {
+  console.log(event.target.value);
+
+  const uf = event.target.value
+
+  fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${uf}/municipios`)
+    .then(response => response.json())
+    .then(json => {
+      console.log(json)
+
+      let options = '<option selected disabled>Selecione...</option>'
+
+      json.forEach(municipio => {
+        options = options + `<option value="${municipio.nome}">${municipio.nome}</option>`
+      })  
+
+      document.getElementById('localidade').innerHTML = options 
+
+    })    
+  
+}
+
+//fetchMunicipios()
